bc5e21420e36d698cade60928bfde475fdcdd842
tonnylyz <lyztonny@gmail.com>
2019-06-09 15:28:13 +0800

    Refine type decl

--- src/page/article.rs
+++ src/page/article.rs
@@ -1,24 +1,23 @@
 use crate::page_context::{PageContext, PageContent, ArticleItem};
 use rocket_contrib::templates::Template;
+use chrono::NaiveDateTime;
 
 #[get("/article?<id>")]
 pub fn article(id: u32) -> Option<Template> {
     let pool = mysql::Pool::new(crate::config::conn()).unwrap();
-    let post: Option<ArticleItem> = pool.first_exec("SELECT id, title, description, datetime, tag, content FROM article WHERE id = :id", params! {"id" => id})
+    let post = pool.first_exec("SELECT id, title, description, datetime, tag, content FROM article WHERE id = :id", params! {"id" => id})
         .map(|result| {
             match result {
                 Some(row) => {
-                    let (id, title, description, datetime, tag, content) = mysql::from_row(row);
-                    let tag_string: String = tag;
-                    let tags = tag_string.split('|').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
-                    let content_md: String = content;
+                    let (id, title, description, datetime, tag, content) = mysql::from_row::<(u32, String, String, NaiveDateTime, String, String)>(row);
+                    let tags = tag.split('|').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
                     Some(ArticleItem {
                         id,
                         title,
                         description,
                         datetime,
                         tag: tags,
-                        content: content_md,
+                        content,
                     })
                 }
                 None => None
--- src/page/blog.rs
+++ src/page/blog.rs
@@ -1,16 +1,16 @@
 use crate::page_context::{PageContext, PageContent, BlogListItem};
 use rocket_contrib::templates::Template;
+use chrono::NaiveDateTime;
 
 #[get("/blog")]
 pub fn blog() -> Template {
     let pool = mysql::Pool::new(crate::config::conn()).unwrap();
-    let blog_list: Vec<BlogListItem> =
+    let blog_list =
         pool.prep_exec("SELECT id, title, description, datetime, tag FROM article ORDER BY id DESC", ())
             .map(|result| {
                 result.map(|x| x.unwrap()).map(|row| {
-                    let (id, title, description, datetime, tag) = mysql::from_row(row);
-                    let tag_string: String = tag;
-                    let tags = tag_string.split('|').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
+                    let (id, title, description, datetime, tag) = mysql::from_row::<(u32, String, String, NaiveDateTime, String)>(row);
+                    let tags = tag.split('|').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
                     BlogListItem {
                         id,
                         title,
@@ -18,7 +18,7 @@ pub fn blog() -> Template {
                         datetime,
                         tag: tags,
                     }
-                }).collect()
+                }).collect::<Vec<_>>()
             }).unwrap();
 
     Template::render("blog", &PageContext {
